b31014328026f6085153aa104295d4490191f74e
Vokath <112853011+Vokath@users.noreply.github.com>
2022-10-16 19:11:56 +0300

    Add files via upload

---
+++ prototyyp.py
@@ -0,0 +1,21 @@
+import tweepy
+
+
+consumer_key = "" #REDACTED
+consumer_secret = "" #REDACTED
+access_token = "" #REDACTED
+access_token_secret = "" #REDACTED
+
+
+client = tweepy.Client(
+    consumer_key=consumer_key, consumer_secret=consumer_secret,
+    access_token=access_token, access_token_secret=access_token_secret
+)
+
+# Create Tweet
+
+
+response = client.create_tweet(
+    text="This Tweet was Tweeted using Tweepy and Twitter API v2!"
+)
+print(f"https://twitter.com/user/status/{response.data['id']}")
